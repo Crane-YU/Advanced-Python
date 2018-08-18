ff3d8aae1568e85720063ec4a62c70129c84a2cc
Crane-YU <jianhuiyu2@gmail.com>
2018-08-18 14:25:16 +1000

    normal update

--- multithread_test.py
+++ multithread_test.py
@@ -4,22 +4,50 @@
 # we use threading instead of _thread
 
 import time, threading
+from threading import Thread
 
 
-# new threading function block, running after the main thread
-def loop():
-    print("thread %s is running" % threading.current_thread().name)
-    n = 0
-    while n < 5:
-        n += 1
-        print('thread %s >>> %s' % (threading.current_thread().name, n))
-        time.sleep(1)
+# Method1 to create the thread
+def sayHi(name):
+    time.sleep(2)
+    print("%s says hello" %name)
 
-    print("thread %s ends" % threading.current_thread().name)
+if __name__ == '__main__':
+    t = Thread(target=sayHi, args=("Crane",))
+    t.start()
+    print("This is the main thread.")
 
 
-print('thread %s is running' % threading.current_thread().name)
-t = threading.Thread(target=loop, name='LoopThread')
-t.start()
-t.join()
-print('thread %s ended' % threading.current_thread().name)
+# Method2 to create the thread
+class Sayhi(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        time.sleep(2)
+        print("%s says hello" %self.name)
+
+if __name__ == '__main__':
+    t = Sayhi("Crane")
+    t.start()
+    print("This is the main thread.")
+
+
+# New threading function block, running after the main thread
+# def loop():
+#     print("thread %s is running" % threading.current_thread().name)
+#     n = 0
+#     while n < 5:
+#         n += 1
+#         print('thread %s >>> %s' % (threading.current_thread().name, n))
+#         time.sleep(1)
+#
+#     print("thread %s ends" % threading.current_thread().name)
+#
+#
+# print('thread %s is running' % threading.current_thread().name)
+# t = threading.Thread(target=loop, name='LoopThread')
+# t.start()
+# t.join()
+# print('thread %s ended' % threading.current_thread().name)
